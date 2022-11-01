367b81009f25ce7b66a606212a5b55eb214577d8
Poseidoncode <posidomhu@gmail.com>
2022-11-02 00:39:24 +0800

    test

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -8,7 +8,7 @@
           alt=""
         />
         <div class="p-6 flex flex-col justify-start">
-          <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+          <h5 class="text-gray-900 text-xl font-medium mb-2">Card title２</h5>
           <p class="text-gray-700 text-base mb-4">
             This is a wider card with supporting text below as a natural lead-in to
             additional content. This content is a little bit longer.
