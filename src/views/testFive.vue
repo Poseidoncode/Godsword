6db1bafdb8b527bf5a26bd497083400a48248d31
Poseidoncode <posidomhu@gmail.com>
2022-11-06 21:59:11 +0800

    mode

--- src/views/testFive.vue
+++ src/views/testFive.vue
@@ -1,11 +1,11 @@
 <template>
   <div>Page5</div>
 
-  <div @click="$router.push('/testone')">test1</div>
-  <div @click="$router.push('/testtwo')">test2</div>
-  <div @click="$router.push('/testthree')">test3</div>
-  <div @click="$router.push('/testFour')">test4</div>
-  <div @click="$router.push('/testFive')">test5</div>
+  <div class="cursor-pointer" @click="$router.push('/testone')">test1</div>
+  <div class="cursor-pointer" @click="$router.push('/testtwo')">test2</div>
+  <div class="cursor-pointer" @click="$router.push('/testthree')">test3</div>
+  <div class="cursor-pointer" @click="$router.push('/testFour')">test4</div>
+  <div class="cursor-pointer" @click="$router.push('/testFive')">test5</div>
 </template>
 
 <script setup></script>
--- src/views/testone.vue
+++ src/views/testone.vue
@@ -1,13 +1,33 @@
 <template>
   <div>Page1-123</div>
 
-  <div @click="$router.push('/testone')">test1</div>
-  <div @click="$router.push('/testtwo')">test2</div>
-  <div @click="$router.push('/testthree')">test3</div>
-  <div @click="$router.push('/testFour')">test4</div>
-  <div @click="$router.push('/testFive')">test5</div>
+  <div class="cursor-pointer" @click="$router.push('/testone')">test1</div>
+  <div class="cursor-pointer" @click="$router.push('/testtwo')">test2</div>
+  <div class="cursor-pointer" @click="$router.push('/testthree')">test3</div>
+  <div class="cursor-pointer" @click="$router.push('/testFour')">test4</div>
+  <div class="cursor-pointer" @click="$router.push('/testFive')">test5</div>
+
+  <button @click="setSpecificEvent">setSpecificEvent</button>
 </template>
 
-<script setup></script>
+<script setup>
+import { event } from "vue-gtag";
+
+const login = () => {
+  // event('login', { method: 'Google' })
+  event("aaa", {
+    event_category: "bbb",
+    event_label: "ccc",
+  });
+};
+
+const setSpecificEvent = () => {
+  console.log("setSpecificEvent");
+  event("aaa", {
+    event_category: "bbb",
+    event_label: "ccc",
+  });
+};
+</script>
 
 <style lang="scss" scoped></style>
